15616ae5f0926c86976a9da368b674501d0bb6f8
paisley-laurenzio <paisleylaurenzio@Paisleys-MacBook-Pro.local>
2015-03-14 10:15:22 -0400

    trying to figure out why lightbox won't work

--- js/main.js
+++ js/main.js
@@ -12,5 +12,20 @@ document.querySelector('.nav-btn').addEventListener('click', function (e) {
     navTop.setAttribute('data-state', 'expanded');
     this.setAttribute('data-state', 'active');
   }
-    console.log('clicked');
+});
+
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox-img');
+var $lb = $('.lightbox');
+var $btnClose = $('.btn-close');
+
+$thumbs.on('click', 'a', function (e){
+    e.preventDefault();
+    var big = $(this).attr ('href');
+    $lbImg.attr('src', big);
+    $lb.attr('data-state', 'visible');
+});
+
+btnClose.on('click', function (){
+    $lb.attr('data-state','visible');
 });
