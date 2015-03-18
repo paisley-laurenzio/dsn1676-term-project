251a35378ed2cd4298bc833a049ef8da23584e44
paisley-laurenzio <paisleylaurenzio@Paisleys-MacBook-Pro.local>
2015-03-18 13:32:59 -0400

    Fixed js spacing and now light box works

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 var navTop = document.querySelector('.nav-top');
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox-img');
+var $lb = $('.lightbox');
+var $btnClose = $('.btn-close');
 
 document.querySelector('.nav-btn').addEventListener('click', function (e) {
 
@@ -15,25 +19,13 @@ document.querySelector('.nav-btn').addEventListener('click', function (e) {
 
 
 
-interact('.tap-target')
-  .on('tap', function (event) {
-    event.currentTarget.classList.toggle('switch-bg');
-    event.preventDefault();
-  });
-
-
-var $thumbs = $('.thumbnails');
-var $lbImg = $('.lightbox-img');
-var $lb = $('.lightbox');
-var $btnClose = $('.btn-close');
-
 $thumbs.on('click', 'a', function (e){
     e.preventDefault();
-    var big = $(this).attr ('href');
+    var big = $(this).attr('href');
     $lbImg.attr('src', big);
     $lb.attr('data-state', 'visible');
 });
 
-btnClose.on('click', function (){
-    $lb.attr('data-state','hidden');
+$btnClose.on('click', function (){
+    $lb.attr('data-state', 'hidden');
 });
